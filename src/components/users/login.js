dc0c4866289b8f10225a7655b073c74f79d2789a
LuizaAv <l.avetisyan7777@gmail.com>
2023-12-13 00:47:10 +0400

    added email and password inputs change handling with react hooks

--- src/components/users/login.js
+++ src/components/users/login.js
@@ -1,22 +1,44 @@
-
-
-
-
+import { useState } from "react";
 import "./login.css";
 import { Link } from "react-router-dom";
 
-export default function Login(){
-    return(
-        <div className="loginContainer">
-            <h1>Login form</h1>
-            <form className = "loginForm">
-                <label>Name</label>
-                <input type = "text" placeholder = "...enter an email" className="loginInput"/>
-                <label>Surename</label>
-                <input type = "text" placeholder = "...enter the password" className="loginInput"/>
-                <input type = "submit" value = "Submit" className = "loginSubmit"/>
-            </form>
-            <Link to = "/register" target="_blank" className="loginLink">Do you forget password?</Link>
-        </div>
-    )
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  return (
+    <div className="loginContainer">
+      <h1>Login form</h1>
+      <form className="loginForm">
+        <label>Email</label>
+        <input
+          type="text"
+          placeholder="...enter an email"
+          value = {email}
+          onChange={handleEmail}
+          className="loginInput"
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          placeholder="...enter the password"
+          value={password}
+          onChange={handlePassword}
+          className="loginInput"
+        />
+        <input type="submit" value="Submit" className="loginSubmit" />
+      </form>
+      <Link to="/register" target="_blank" className="loginLink">
+        Do you forget password?
+      </Link>
+    </div>
+  );
 }
